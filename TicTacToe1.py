2bdd980578ba2bde68bc6c81ea0c55050d20a503
Shrey Suri <shreysuri5@gmail.com>
2022-05-09 22:33:41 -0500

    needs some trimming

--- TicTacToe1.py
+++ TicTacToe1.py
@@ -11,7 +11,7 @@ while symbol_1 == symbol_2:
         symbol_1 = input(print("Player 1, choose your symbol: circles, crosses, or hearts. "))
         symbol_1 = symbol_1.lower()
         for i in range (0, symbols_count):
-            if symbol[i] == symbol_1:
+            if symbols[i] == symbol_1:
                 check_1 = check_1 + 1
             else:
                 check_1 = check_1 + 0
@@ -19,10 +19,29 @@ while symbol_1 == symbol_2:
         symbol_2 = input(print("Player 2, choose your symbol: circles, crosses, or hearts. "))
         symbol_2 = symbol_2.lower()
         for i in range (0, symbols_count):
-            if symbol[i] == symbol_1:
+            if symbols[i] == symbol_1:
                 check_1 = check_1 + 1
             else:
                 check_1 = check_1 + 0
             
+import turtle
 
-S
+t_grid = turtle.Pen()
+t_grid.hideturtle()
+t_grid.up()
+
+width = 100
+
+t_grid.forward(width * 1.5)
+t_grid.left(90)
+t_grid.forward(width * 0.5)
+t_grid.left(90)
+for i in range (1,5):
+    t_grid.down()
+    t_grid.forward(width * 3)
+    t_grid.up()
+    t_grid.right(90)
+    for j in range (1,3):
+        t_grid.forward(width)
+        t_grid.right(90)
+    
