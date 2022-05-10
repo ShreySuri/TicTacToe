177735bdec18002909b42374ed96d64805f038ba
Shrey Suri <shreysuri5@gmail.com>
2022-05-09 22:17:16 -0500

    starting to code

---
+++ TicTacToe1.py
@@ -0,0 +1,11 @@
+symbols = ["circles", "crosses", "hearts"]
+symbols_count = 3
+
+symbol_1 = None
+symbol_2 = None
+
+symbol_1 = input(print("Player 1, choose your symbol: circles, crosses, or hearts. "))
+symbol_1 = symbol_1.lower()
+for i in range 
+symbol_2 = input(print("Player 2, choose your symbol: circles, crosses, or hearts. "))
+symbol_2 = symbol_2.lower()
