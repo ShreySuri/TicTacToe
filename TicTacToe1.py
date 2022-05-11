bd73d1cbdc01596e7e6e31bbb2666c0ea7b58bac
Shrey Suri <shreysuri5@gmail.com>
2022-05-10 22:08:02 -0500

    finished with first part

--- TicTacToe1.py
+++ TicTacToe1.py
@@ -6,7 +6,7 @@ symbol_2 = None
 check_1 = 0
 check_2 = 0
 
-while symbol_1 == symbol_2:
+while check_1 == 0 and check_2 == 0:
     while check_1 == 0:
         symbol_1 = input(print("Player 1, choose your symbol: circles, crosses, or hearts. "))
         symbol_1 = symbol_1.lower()
@@ -19,10 +19,10 @@ while symbol_1 == symbol_2:
         symbol_2 = input(print("Player 2, choose your symbol: circles, crosses, or hearts. "))
         symbol_2 = symbol_2.lower()
         for i in range (0, symbols_count):
-            if symbols[i] == symbol_1:
-                check_1 = check_1 + 1
+            if symbols[i] == symbol_2:
+                check_2 = check_2 + 1
             else:
-                check_1 = check_1 + 0
+                check_2 = check_2 + 0
             
 import turtle
 
