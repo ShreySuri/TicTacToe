433033b0ed3de86db5972343254a3ff6766af79d
Shrey Suri <shreysuri5@gmail.com>
2022-05-11 22:43:54 -0500

    coding for checker

--- TicTacToe1.py
+++ TicTacToe1.py
@@ -53,15 +53,25 @@ marker.forward(width * 1.5)
 marker.right(90)
 marker.forward(width * 1.5)
 marker.right(90)
+squares = [1, 2, 3, 4, 5,  6, 7, 8, 9]
+vacant_squares = 9
 
 win = True
+vacancy = False
 
 while win == True:
     guess_1 = 0
-    while guess_1 % 1 != 0 or guess_1 < 1 or guess_1 > 10:
+    while guess_1 % 1 != 0 or guess_1 < 1 or guess_1 > 10 or vacancy = False:
         guess_1 = input(print("Player 1, which square would you like to mark? 1 - 9. "))
+        for i in range (0, vacant_squares):
+            
+    guess_1 = guess_1 - 1
     x = guess_1 % 3
     y = int((guess_1 - x)/3)
-    coordinate = x + 10 * y
+    marker.forward(width * x)
+    marker.right(90)
+    marker.forward(width * y)
+    marker.left(90)
+    
         
                     
