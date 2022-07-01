36345da5c210e20f5fd5c7597b5a71c5ce3b66db
Shrey Suri <shreysuri5@gmail.com>
2022-06-30 22:41:16 -0500

    new test file

--- test_file_2.py
+++ test_file_2.py
@@ -1,6 +1,40 @@
-x = 0
-y = 1
-xy = x + y * 0.1
+import turtle
+
+marker = turtle.Pen()
+
+width = 200
+space = 10
 
 
-print(float(xy))
+
+
+marker.left(90)
+for i in range (0, 5):
+    marker.forward(width)
+    marker.right(90)
+
+
+
+
+
+
+fill_color = red
+space = space * 2
+marker.up()
+marker.forward(space)
+marker.right(90)
+marker.forward(space)
+marker.left(90)
+marker.down()
+marker.begin_fill(red)
+length = width - 2 * space
+for i in range (0, 4):
+    marker.forward(length)
+    marker.right(90)
+marker.end_fill()
+marker.up()
+marker.left(90)
+marker.forward(space)
+marker.left(90)
+marker.forward(space)
+space = int(space * 0.5)
