0eaa35eb19fa21f9cbefc11b1d9f03617113dc80
Shrey Suri <shreysuri5@gmail.com>
2022-07-01 11:36:34 -0500

    working

--- test_file_2.py
+++ test_file_2.py
@@ -1,39 +1,20 @@
-import turtle
-
-marker = turtle.Pen()
-
-width = 200
-space = 10
 
 
 
+    def_counter = 0
+    bool_def_counter = False
+    for i in range (0, 3):
+        target_rem = i * 0.1
+        for i in range (0, counter):
+            rem = list_1[i] % 1
+            if rem == target_rem:
+                def_counter = def_counter + 1
+            else:
+                def_counter = def_counter + 0
 
-marker.left(90)
-for i in range (0, 5):
-    marker.forward(width)
-    marker.right(90)
+        if def_counter == 3:
+            bool_def_counter = True
+        else:
+            bool_def_counter = bool_def_counter
 
-
-
-
-
-
-space = space * 2
-marker.up()
-marker.forward(space)
-marker.right(90)
-marker.forward(space)
-marker.left(90)
-marker.down()
-marker.begin_fill(red)
-length = width - 2 * space
-for i in range (0, 4):
-    marker.forward(length)
-    marker.right(90)
-marker.end_fill()
-marker.up()
-marker.left(90)
-marker.forward(space)
-marker.left(90)
-marker.forward(space)
-space = int(space * 0.5)
+    return(bool_def_counter)
